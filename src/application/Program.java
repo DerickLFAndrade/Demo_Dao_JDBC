9df1e187bfbbc19c34adcf887d3ea1694f68b1eb
DerickLFAndrade <derickspy15@gmail.com>
2023-01-21 02:54:45 -0300

    insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -29,6 +30,14 @@ public class Program {
 		for (Seller sl : list) {
 			System.out.println(sl);
 		}
+		
+		
+		System.out.println("\n=== Teste 4: seller findById ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		
+		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
 	}
 
 }
